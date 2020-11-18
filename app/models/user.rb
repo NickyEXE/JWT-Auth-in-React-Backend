7368050e1885539bc710ba99164a66b8de80ac6c
NickyEXE <dovern42@gmail.com>
2020-11-18 15:09:57 -0500

    end of test 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :messages
   has_many :channels, through: :messages
+  has_many :likes
   has_secure_password
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_233503) do
+ActiveRecord::Schema.define(version: 2020_11_18_200438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2020_11_17_233503) do
     t.bigint "message_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["message_id"], name: "index_likes_on_message_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -46,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_233503) do
   end
 
   add_foreign_key "likes", "messages"
+  add_foreign_key "likes", "users"
   add_foreign_key "messages", "channels"
   add_foreign_key "messages", "users"
 end
